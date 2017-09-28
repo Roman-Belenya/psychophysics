26f2cedd0d56040b12715c5e7a99f0a62eddf110
Roman Belenya <romabelenya@gmail.com>
2017-09-28 10:00:40 -0500

    small changes

--- determineColourDifference.py
+++ determineColourDifference.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 pic = np.zeros([8, 8, 3], dtype = np.float32)
 
-win = visual.Window(monitor = 'testMonitor', fullscr = True, units = 'pix', colorSpace = 'rgb', color = 0, size = [1920, 1080])
+win = visual.Window(monitor = 'testMonitor', fullscr = True, units = 'pix', colorSpace = 'rgb', color = 0, size = [1920, 1080], useFBO = True)
 stim = visual.GratingStim(win, tex = pic, size = [1920, 1080], colorSpace = 'rgb')
 lum = visual.TextStim(win, text = '0', pos = (0, 0), colorSpace = 'rgb', color = -1)
 
@@ -21,8 +21,8 @@ while not finished:
         pic[:, 0:4, :] += delta
     elif key[0] == 'down':
         pic[:, 0:4, :] -= delta
-    elif key[0] == 'escape':
-        finished = True
+
+    finished = key[0] == 'escape'
 
     lum.text = str(pic[0,0,0])
     stim.tex = pic
@@ -35,7 +35,7 @@ win.close()
 
 
 pic = np.zeros([1080, 1920, 3], dtype = np.uint8) + 128
-pic[:, 0:540, :] += 100
+pic[:, 0:960, :] += 1
 Image.fromarray(pic).save('compare.png', 'PNG')
 
 
