2cc95df8a4ed441758de4bf62e265839855924c4
Roman Belenya <romabelenya@gmail.com>
2018-01-10 13:01:53 -0600

    rgb entries in define colours popup

--- app.py
+++ app.py
@@ -21,49 +21,56 @@ class PopupEntries(object):
         self.def_col = def_col
         self.finished = False
 
-        self.grey_var = tk.StringVar(); self.grey_var.set('[0, 0, 0]')
-        self.col_var = tk.StringVar(); self.col_var.set('[0, 0, 0]')
+
+        self.grey_vars = {'r': tk.IntVar(), 
+                          'g': tk.IntVar(),
+                          'b': tk.IntVar()}
+                          
+        self.col_vars = {'r': tk.IntVar(), 
+                  'g': tk.IntVar(),
+                  'b': tk.IntVar()}
+                  
+        entry_params = {'width': 3, 'relief': tk.FLAT, 'justify': tk.CENTER}
+        grid_params = {'padx': 5, 'pady': 5}
+        
+        for i, n in enumerate(['R', 'G', 'B']):
+            tk.Label(self.top, text = n).grid(row = 0, column = i+1, sticky = 'WE', pady = 5)
 
         if def_grey:
-            tk.Label(self.top, text = 'Contrast (FG):').grid(row = 0, column = 0, sticky = 'E', pady = 5)
-            self.grey_entry = tk.Entry(self.top,
-                textvariable = self.grey_var,
-                width = 15,
-                justify = tk.CENTER,
-                relief = tk.FLAT)
-            self.grey_entry.grid(row = 0, column = 1, padx = (10, 0), pady = 5)
+            tk.Label(self.top, text = 'Contrast (FG grey):').grid(row = 1, column = 0, sticky = 'E', pady = 5)
+            for i, col in enumerate(['r', 'g', 'b']):
+                tk.Entry(self.top, 
+                    textvariable = self.grey_vars[col], 
+                    **entry_params).grid(row = 1, column = i+1, **grid_params)
 
         if def_col:
-            tk.Label(self.top, text = 'Isoluminance (BG):').grid(row = 1, column = 0, sticky = 'E', pady = 5)
-            self.col_entry = tk.Entry(self.top,
-                textvariable = self.col_var,
-                width = 15,
-                justify = tk.CENTER,
-                relief = tk.FLAT)
-            self.col_entry.grid(row = 1, column = 1, padx = (10, 0), pady = 5)
+            tk.Label(self.top, text = 'Isoluminance (BG col):').grid(row = 2, column = 0, sticky = 'E', pady = 5)
+            for i, col in enumerate(['r', 'g', 'b']):
+                tk.Entry(self.top, 
+                    textvariable = self.col_vars[col], 
+                    **entry_params).grid(row = 2, column = i+1, **grid_params)
 
         tk.Button(self.top,
             text = 'OK',
             command = self.ok,
             width = 5,
-            relief = tk.GROOVE).grid(row = 2, column = 1, pady = (10, 0))
+            relief = tk.GROOVE).grid(row = 3, column = 1, columnspan = 3, pady = (15, 0))
             
         tk.Button(self.top,
             text = 'Load colours',
             command = self.load_colours,
             width = 10,
-            relief = tk.GROOVE).grid(row = 2, column = 0, pady = (10,0))
+            relief = tk.GROOVE).grid(row = 3, column = 0, pady = (15,0))
 
 
     def ok(self):
 
         try:
-            grey = ast.literal_eval(self.grey_var.get())
-            col = ast.literal_eval(self.col_var.get())
+            grey = [self.grey_vars['r'].get(), self.grey_vars['g'].get(), self.grey_vars['b'].get()]
+            col = [self.col_vars['r'].get(), self.col_vars['g'].get(), self.col_vars['b'].get()]
             for arr in [grey, col]:
-                assert len(arr) == 3
                 assert all(0 <= i <= 255 for i in arr)
-        except:
+        except AssertionError:
             showwarning('Entry', 'Incorrect value')
             return
 
@@ -72,8 +79,10 @@ class PopupEntries(object):
 
         if self.def_col:
             self.master.colours['bg_col'] = col
-
-        self.finished = True
+        
+        print 'grey:', grey
+        print 'col:', col
+        self.finished = False
         self.top.grab_release()
         self.top.destroy()
         
