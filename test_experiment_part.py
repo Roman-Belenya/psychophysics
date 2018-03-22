7745dfb59a3d33f7d362c57358c4eb6f3f3f80ce
Roman <romabelenya@gmail.com>
2018-03-21 20:46:45 -0500

    fixed refresh rate bug by updating the driver; better testing

--- test_experiment_part.py
+++ test_experiment_part.py
@@ -25,10 +25,11 @@ class TestExperimentPart(unittest.TestCase):
             screen = 0,
             allowGUI = False,
             fullscr = True,
-            waitBlanking = False,
             colorSpace = 'rgb255',
             color = 128,
             units = 'deg')
+        cls.win.recordFrameIntervals = True
+
 
         cls.instructions = visual.TextStim(win = cls.win, pos = (0, 15), color = 255)
 
@@ -42,6 +43,7 @@ class TestExperimentPart(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        cls.win.winHandle.minimize()
         cls.win.close()
         # shutil.rmtree('./__test__/')
         logger.removeHandler(cls.stream_handler)
@@ -79,18 +81,6 @@ class TestExperimentPart(unittest.TestCase):
                     self.assertIn(stim, stims, 'Incorrect stim name in seq_file: {}'.format(stim))
 
 
-    def test_check_colours_dict(self):
-        del self.choice.colours_dict['fg_grey']
-        self.assertRaises(AssertionError, self.choice.check_colours_dict)
-        self.choice.colours_dict['fg_grey'] = [128, 128, 128]
-
-        self.choice.colours_dict['bg_col'] = [256, 0, 0]
-        self.assertRaises(AssertionError, self.choice.check_colours_dict)
-
-
-        self.choice.colours_dict['bg_col'] = [0, 0, 255]
-
-
     def test_frame_rate(self):
         mon_fs = self.win.monitor.refresh_rate
         self.win.refreshThreshold = 1./mon_fs + 0.004
@@ -101,12 +91,9 @@ class TestExperimentPart(unittest.TestCase):
         self.assertEqual(mon_fs, round(framerate), 'Incorrect monitor frame rate: {}, actual is {}'.format(mon_fs, framerate))
         logger.info('actual framerate is {}'.format(framerate))
 
-    def test_flicker_fs(self):
-        mfs = self.win.monitor.refresh_rate
-        ffs = self.isolum.flicker_fs
-        self.assertLessEqual(2 * ffs, mfs, 'Flicker rate should be at least twice as little as monitor fs')
 
     def test_timing(self):
+        self.win.nDroppedFrames = 0
         frame = 0
         t = 10
         stim = visual.TextStim(win = self.win, text = '0', pos = (0,0))
@@ -117,33 +104,79 @@ class TestExperimentPart(unittest.TestCase):
             self.win.flip()
             frame += 1
         dt = time.time() - t0
-        self.assertAlmostEqual(dt, t, delta = 0.5, msg = 'Large timing error: {}, should be {} sec'.format(dt, t))
-        logger.info('timing error is {}'.format(dt - t))
 
+        logger.info('dropped {} frames: {}%'.format(self.win.nDroppedFrames, self.win.nDroppedFrames*100.0/frame))
+        logger.info('timing error is {}'.format(dt - t))
+        self.assertAlmostEqual(dt, t, delta = 0.5, msg = 'Large timing error: {}, should be {} sec'.format(dt, t))
+        
+        
     def test_flicker(self):
-        self.win.recordFrameIntervals = True
+        self.win.nDroppedFrames = 0
         img = os.path.join('.', 'images', 'circle.png')
         fg = get_fg_mask(img)
         self.isolum.stim.mask = fg
         self.isolum.col_delta = np.array([0, 1, 0])
-        col = self.isolum.run_trial([0, 120, 0])
+        colour = self.params['default_colours']['bg_col']
+        half_cycle = self.win.monitor.refresh_rate / (2.0 * self.isolum.flicker_fs)
+        frame = 0
+        n = 0
+        t = 10
 
+        while frame < self.win.monitor.refresh_rate * t:
+            if frame % half_cycle == 0:
+                if all(self.isolum.stim.color == self.isolum.fix_col):
+                    self.isolum.stim.color = colour
+                    n += 1
+                else:
+                    self.isolum.stim.color = self.isolum.fix_col
+            self.isolum.stim.draw()
+            self.win.flip()
+            frame += 1
+
+            ans = event.getKeys(keyList = ['up', 'down', 'return'])
+            if ans:
+                if ans[0] == 'up':
+                    colour = change_colour(colour, self.isolum.col_delta)
+                elif ans[0] == 'down':
+                    colour = change_colour(colour, -1*self.isolum.col_delta)
+                elif ans[0] == 'return':
+                    logger.info('Colours are {} and {}'.format(colour, self.isolum.fix_col))
+
+        cycles_per_sec = float(n) / (float(frame) / self.win.monitor.refresh_rate)
         msperframe = 1000. / self.win.monitor.refresh_rate
         fints = np.array(self.win.frameIntervals) * 1000
         t1 = fints.mean() - fints.std()
         t2 = fints.mean() + fints.std()
-        self.assertTrue(t1 < msperframe < t2, 'Strange refresh period ({}, should be {})'.format(fints.mean(), msperframe))
-        self.assertLess(self.win.nDroppedFrames, 5, msg = 'Too many dropped frames ({})'.format(self.win.nDroppedFrames))
+        
+        logger.info('Flicker frequency is {}'.format(cycles_per_sec))
+        logger.info('dropped {} frames: {}%'.format(self.win.nDroppedFrames, self.win.nDroppedFrames*100.0/frame))
         logger.info('{} +- {} ms to refresh each frame, should be {}'.format(fints.mean(), fints.std(), msperframe))
-        logger.info('dropped {} frames'.format(self.win.nDroppedFrames))
+        
+        self.assertTrue(t1 < msperframe < t2, 'Strange refresh period ({}, should be {})'.format(fints.mean(), msperframe))
+        self.assertEqual(self.isolum.flicker_fs, cycles_per_sec, 'Strange flicker rate: set to {}, but actually is {}'.format(self.isolum.flicker_fs, cycles_per_sec))
+        self.assertLessEqual(2.0 * self.isolum.flicker_fs, self.win.monitor.refresh_rate, 'Flicker frequency cannot be greater than half the monitor fs: set to {}'.format(self.isolum.flicker_fs))
+        self.assertLess(self.win.nDroppedFrames, frame*0.05, msg = 'Too many dropped frames ({})'.format(self.win.nDroppedFrames))
+
 
     def test_gamma(self):
-        bg = visual.GratingStim(win = self.win, tex = None, size = 500, color = 0, colorSpace = 'rgb255', units = 'pix')
-        stim = visual.GratingStim(win = self.win, tex = None, mask = 'circle', size = 200, color = 1, colorSpace = 'rgb255', units = 'pix')
+        self.win.nDroppedFrames = 0
+        t = 5.0
+        width_pix = self.win.size[0]
+        bg = visual.GratingStim(win = self.win, tex = None, size = self.win.size, color = 0, colorSpace = 'rgb255', units = 'pix')
+        stim = visual.GratingStim(win = self.win, tex = None, size = 200, color = 1, colorSpace = 'rgb255', units = 'pix', pos = [-width_pix/2.0, 0])
         bg.draw()
         stim.draw()
         self.win.flip()
-        event.waitKeys()
+        clock = core.Clock()
+        pp = width_pix/(t * self.win.monitor.refresh_rate)
+        while clock.getTime() < t:
+            stim.pos[0] += pp
+            stim.ori += 360.0*5 / (t * self.win.monitor.refresh_rate)
+            bg.draw()
+            stim.draw()
+            self.win.flip()
+            
+        logger.info('dropped {} frames: {}%'.format(self.win.nDroppedFrames, self.win.nDroppedFrames*100.0/(t*self.win.monitor.refresh_rate)))
 
 
 
